5d84402c597d4d6e60600088fcf0d5c1f25e096d
quocthinhvo <quocthinhvo0@gmail.com>
2021-08-14 18:57:34 +0700

    Fix button send layout

--- test.js
+++ test.js
@@ -1,8 +1,6 @@
 //const database = require('./controller/mongo')
 const setup = require('./controller/setup')
 const token = require('./middleware/token')
-//setup.createAdminAccount("admin", "admin@12345")
 
-token.getData('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiaWF0IjoxNjI4ODUzNzIzLCJleHAiOjE2MzQwMzc3MjN9.YO8Ovm0T0BqSlCWmMq5tFvpkhb42S-oPVXDFJSb3PO8', (cc, bb)=>{
-    console.log(cc)
-})
+console.log(setup.renderTokenKey())
+console.log(setup.renderTokenKey())
